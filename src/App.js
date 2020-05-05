7818660aa72f20fe888f879b555a2796c0a8d1f5
mestrpra <mestry.prashant10@gmail.com>
2020-05-05 21:55:37 +0530

    few lines added

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={logo} className="App-logo" alt="logo" />        
         <h3>First demo for netlify and buddy work</h3>
-        <p>work with passion</p>        
+        <p>work with passion</p> 
+        <p>/Users/mestrpra/REACT/react-demo</p>       
+        <p>https://app.netlify.com/sites/jolly-jones-1aca2b/settings/deploys</p>
+        <p>https://app.buddy.works/mestryprashant10/react-demo/pipelines</p>
+        <p>https://jolly-jones-1aca2b.netlify.app/</p>
       </header>
     </div>
   );
